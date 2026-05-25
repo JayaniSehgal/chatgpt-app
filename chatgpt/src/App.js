38ac074d1eb1332d6a4e671f9c991546f40b74d4
Jayani Sehgal <jayanisehgal15@gmail.com>
2026-05-25 17:07:25 +0530

    Add React app files properly

---
+++ chatgpt/src/App.js
@@ -0,0 +1,34 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="sidebar">
+        <div className="upperSide">
+          <div className="upperSideTop">
+            <img src="" alt="" className="logo" />
+            <span className="brand">ChatGPT</span>
+            <button className="midBtn">
+              <img src="" alt="" className="addBtn" />
+              New Chat
+            </button>
+          </div>
+          <div className="upperSideBottom">
+            <button className="query">
+              <img src="" alt="" />
+              What is programming?
+            </button>
+            <button className="query">
+              <img src="" alt="" />
+              What is programming?
+            </button>
+          </div>
+        </div>
+        <div className="lowerSide"></div>
+      </div>
+      <div className="main"></div>
+    </div>
+  );
+}
+
+export default App;
